bfa88ab50fdaa73cfe61a53550c5431e6b5a9269
Josh-Corby <85930702+Josh-Corby@users.noreply.github.com>
2024-05-17 11:27:02 +1000

    Combat Target

--- Source/Aura/Public/Interaction/EnemyInterface.h
+++ Source/Aura/Public/Interaction/EnemyInterface.h
@@ -7,7 +7,7 @@
 #include "EnemyInterface.generated.h"
 
 // This class does not need to be modified.
-UINTERFACE(MinimalAPI)
+UINTERFACE(MinimalAPI, BlueprintType)
 class UEnemyInterface : public UInterface
 {
 	GENERATED_BODY()
@@ -20,9 +20,14 @@ class AURA_API IEnemyInterface
 {
 	GENERATED_BODY()
 
-	// Add interface functions to this class. This is the class that will be inherited to implement this interface.
 public:
 
 	virtual void HighlightActor() = 0;
 	virtual void UnHighlightActor() = 0;
+
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable)
+	void SetCombatTarget(AActor* InCombatTarget);
+
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable)
+	AActor* GetCombatTarget() const;
 };
