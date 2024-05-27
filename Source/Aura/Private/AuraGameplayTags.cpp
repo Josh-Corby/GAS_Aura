48a276f7945566a539a5e6d9fa7204749aacf7a1
Josh-Corby <85930702+Josh-Corby@users.noreply.github.com>
2024-05-27 11:07:20 +1000

    Debuff Parameters

--- Source/Aura/Private/AuraGameplayTags.cpp
+++ Source/Aura/Private/AuraGameplayTags.cpp
@@ -216,6 +216,26 @@ void FAuraGameplayTags::InitializeNativeGameplayTags()
 		FString("Debuff for Physical Damage")
 	);
 
+	GameplayTags.Debuff_Chance = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("Debuff.Chance"),
+		FString("Debuff Chance")
+	);
+
+	GameplayTags.Debuff_Damage = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("Debuff.Damage"),
+		FString("Debuff Damage")
+	);
+
+	GameplayTags.Debuff_Duration = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("Debuff.Duration"),
+		FString("Debuff Duration")
+	);
+
+	GameplayTags.Debuff_Frequency = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("Debuff.Frequency"),
+		FString("Debuff Frequency")
+	);
+
 	/*
 	 * Map of Damage Types to Resistances
 	 */
