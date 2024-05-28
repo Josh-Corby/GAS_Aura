e064e8550bdce9c7570ef87bff94d17ac6f54e95
Josh-Corby <85930702+Josh-Corby@users.noreply.github.com>
2024-05-28 10:50:06 +1000

    FireBolt Projectile Spread

--- Source/Aura/Private/AbilitySystem/Abilities/AuraFireBolt.cpp
+++ Source/Aura/Private/AbilitySystem/Abilities/AuraFireBolt.cpp
@@ -1,4 +1,5 @@
 #include "AbilitySystem/Abilities/AuraFireBolt.h"
+#include "Kismet/KismetSystemLibrary.h"
 
 FString UAuraFireBolt::GetDescription(int32 Level)
 {
@@ -89,4 +90,43 @@ FString UAuraFireBolt::GetNextLevelDescription(int32 Level)
 		Cooldown,
 		FMath::Min(Level, NumProjectiles),
 		ScaledDamage);
+}
+
+void UAuraFireBolt::SpawnProjectiles(const FVector& ProjectileTargetLocation, const FGameplayTag& SocketTag, bool bOverridePitch, float PitchOverride, AActor* HomingTarget)
+{
+	const bool bIsServer = GetAvatarActorFromActorInfo()->HasAuthority();
+	if (!bIsServer) return;
+
+	const FVector SocketLocation = ICombatInterface::Execute_GetCombatSocketLocation(
+		GetAvatarActorFromActorInfo(),
+		SocketTag);
+
+	FRotator Rotation = (ProjectileTargetLocation - SocketLocation).Rotation();
+	if (bOverridePitch) Rotation.Pitch = PitchOverride;
+
+	const FVector Forward = Rotation.Vector();
+	const FVector LeftOfSpread = Forward.RotateAngleAxis(-ProjectileSpread / 2.f, FVector::UpVector);
+	const FVector RightOfSpread = Forward.RotateAngleAxis(ProjectileSpread / 2.f, FVector::UpVector);
+
+	//NumProjectiles = FMath::Min(MaxNumProjectiles, GetAbilityLevel());
+	const FVector Offset = SocketLocation + FVector(0.f, 0.f, 10.f);
+	if (NumProjectiles > 1)
+	{
+		const float DeltaSpread = ProjectileSpread / (NumProjectiles - 1);
+		for (int32 i = 0; i < NumProjectiles; i++)
+		{
+			const FVector Direction = LeftOfSpread.RotateAngleAxis(DeltaSpread * i, FVector::UpVector);
+			UKismetSystemLibrary::DrawDebugArrow(GetAvatarActorFromActorInfo(), Offset, Offset + Direction * 75.f, 5.f, FLinearColor::Red, 5.f, 2.f);
+		}
+	}
+	else
+	{
+		// Single projectile
+		UKismetSystemLibrary::DrawDebugArrow(GetAvatarActorFromActorInfo(), Offset, Offset + Forward * 75.f, 5.f, FLinearColor::Blue, 5.f, 2.f);
+
+	}
+
+	UKismetSystemLibrary::DrawDebugArrow(GetAvatarActorFromActorInfo(), SocketLocation, SocketLocation + Forward * 100.f, 5.f, FLinearColor::White, 5.f, 2.f);
+	UKismetSystemLibrary::DrawDebugArrow(GetAvatarActorFromActorInfo(), SocketLocation, SocketLocation + LeftOfSpread * 100.f, 5.f, FLinearColor::Gray, 5.f, 2.f);
+	UKismetSystemLibrary::DrawDebugArrow(GetAvatarActorFromActorInfo(), SocketLocation, SocketLocation + RightOfSpread * 100.f, 5.f, FLinearColor::Gray, 5.f, 2.f);
 }
