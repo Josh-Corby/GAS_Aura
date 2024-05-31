a3ac3b1123b8be470a6ce5b76315001c9dc86334
Josh-Corby <85930702+Josh-Corby@users.noreply.github.com>
2024-05-31 14:13:37 +1000

    Aura Fire Ball

--- Source/Aura/Public/AbilitySystem/Abilities/AuraFireBlast.h
+++ Source/Aura/Public/AbilitySystem/Abilities/AuraFireBlast.h
@@ -6,6 +6,8 @@
 #include "AbilitySystem/Abilities/AuraDamageGameplayAbility.h"
 #include "AuraFireBlast.generated.h"
 
+class AAuraFireBall;
+
 /**
  * 
  */
@@ -15,11 +17,20 @@ class AURA_API UAuraFireBlast : public UAuraDamageGameplayAbility
 	GENERATED_BODY()
 
 public:
+
 	virtual FString GetDescription(int32 Level) override;
 	virtual FString GetNextLevelDescription(int32 Level) override;
+
+	UFUNCTION(BlueprintCallable)
+	TArray<AAuraFireBall*> SpawnFireBalls();
 	
 protected:
 
 	UPROPERTY(EditDefaultsOnly, Category = "FireBlast")
 	int32 NumFireBalls = 12;
+
+private:
+
+	UPROPERTY(EditDefaultsOnly)
+	TSubclassOf<AAuraFireBall> FireBallClass;
 };
