cb5cb3e26d853074034bcc01d89e7fe2d65e09b3
Josh-Corby <85930702+Josh-Corby@users.noreply.github.com>
2024-05-31 13:45:00 +1000

    FireBlast Ability

--- Source/Aura/Public/AuraGameplayTags.h
+++ Source/Aura/Public/AuraGameplayTags.h
@@ -68,6 +68,7 @@ public:
 	FGameplayTag Abilities_Summon;
 	FGameplayTag Abilities_Arcane_ArcaneShards;
 	FGameplayTag Abilities_Fire_FireBolt;
+	FGameplayTag Abilities_Fire_FireBlast;
 	FGameplayTag Abilities_Lightning_Electrocute;
 
 	FGameplayTag Abilities_Passive_HaloOfProtection;
