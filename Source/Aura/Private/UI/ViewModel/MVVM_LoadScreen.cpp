3fab5b99f16a9bacaebbcc7cbb0d607988d328ce
Josh-Corby <85930702+Josh-Corby@users.noreply.github.com>
2024-06-02 14:22:30 +1000

    Enabling the Select Slot Button

--- Source/Aura/Private/UI/ViewModel/MVVM_LoadScreen.cpp
+++ Source/Aura/Private/UI/ViewModel/MVVM_LoadScreen.cpp
@@ -42,7 +42,11 @@ void UMVVM_LoadScreen::NewGameButtonPressed(int32 Slot)
 
 void UMVVM_LoadScreen::SelectSlotButtonPressed(int32 Slot)
 {
-
+	for (const TTuple<int32, UMVVM_LoadSlot*> LoadSlot : LoadSlots)
+	{
+		const bool bEnableButton = LoadSlot.Key != Slot;	
+		LoadSlot.Value->EnableSelectSlotButton.Broadcast(bEnableButton);
+	}
 }
 
 void UMVVM_LoadScreen::LoadData()
